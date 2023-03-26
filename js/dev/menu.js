6f6f8e9309ba6401a9d379681cad57e43a633172
Andrei Teodorescu <andreiteodorescu@Andreis-MacBook-Pro.local>
2023-03-26 16:39:23 +0300

    main menu without the search

--- js/dev/menu.js
+++ js/dev/menu.js
@@ -1,4 +1,7 @@
 $('.menu-btn').on("click", function() {
+    $('.menu-btn').removeClass('menu-btn-active');
+    $(this).addClass('menu-btn-active');
+
     if(!$(this).hasClass('show')) {
         $('body').removeClass('nav-active');
     } else {
